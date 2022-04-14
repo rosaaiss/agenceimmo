b5db4376760efecf034eee24fbd338999bf5e852
rosaaiss <rosaissiou@gmail.com>
2022-04-14 10:01:08 +0200

    suppression

--- test_bdd.js
+++
@@ -1,9 +0,0 @@
-// const mongoose = require('mongoose');
-// mongoose.connect(
-//     'mongodb+srv://Rosa:chatchat1@cluster0.sk4ug.mongodb.net/immo',
-//     { connectTimeoutMS: 3000, socketTimeoutMS: 20000, useNewUrlParser: true, useUnifiedTopology: true }
-// );
-// const db = mongoose.connection;
-// db.once('open', () => {
-//     console.log(`connexion OK !`);
-// });
